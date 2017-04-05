9ae669eaee904d515bf53fb10e3cb9a873267769
Maciek Opala <maciek.opala@gmail.com>
2017-04-05 08:22:43 +0200

    43221417

---
+++ 43221417/src/main/java/lol/Lol1.java
@@ -0,0 +1,7 @@
+package lol;
+
+public class Lol1 {
+  public static void main(String[] args) {
+    System.out.println(1);
+  }
+}
---
+++ 43221417/src/main/java/lol/Lol2.java
@@ -0,0 +1,7 @@
+package lol;
+
+public class Lol2 {
+  public static void main(String[] args) {
+    System.out.println(2);
+  }
+}
