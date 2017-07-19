c322dceb8a6ac7f8281cb3ae4c52cf3c5b0b6fe9
Maciek Opala <maciek.opala@gmail.com>
2017-07-19 09:20:26 +0200

    45180264

---
+++ 45180264/src/main/java/org/Lol.java
@@ -0,0 +1,11 @@
+package org;
+
+import com.google.common.base.Optional;
+
+public class Lol {
+
+    public static void main(String[] args) {
+        System.out.println(Optional.of("lol").isPresent());
+    }
+
+}
