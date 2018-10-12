2797fef0c5dbc1be2a84a1073da2aeff1e669df1
Maciek Opala <maciek.opala@gmail.com>
2018-10-12 23:59:10 +0200

    52744893

--- 52744893/src/main/java/lol/Lol.java
+++ 52744893/src/main/java/lol/Lol.java
@@ -1,8 +1,11 @@
 package lol;
 
+import org.eclipse.swt.SWT;
+
 public class Lol {
 
-	public static void main(String []args) {
-		System.out.println("lol!");
-	}
+  public static void main(String[] args) {
+    System.out.println("lol!");
+    System.out.println(SWT.ERROR);
+  }
 }
